9d309a56bd1f695553782a2566e2a8d6b680e582
龚钰涵 <gongyuhan@gongyuhandeMacBook-Air.local>
2026-09-11 16:59:49 +0800

    更新代码

--- data_loader.py
+++ data_loader.py
@@ -19,7 +19,33 @@ def load_attachment_one(path):
 
     workbook.close()
 
-    temperature_data = data[:, [0, 1]]
-    moisture_data = data[:, [0, 2]]
+    #均为{[time,temperature]},{[time,moisture]}，便于后续的线性插值
+    temperature_data = data[:, [0,1]]
+    moisture_data = data[:, [0,2]]
 
     return temperature_data, moisture_data
+
+
+
+def interpolate_every_second(data):
+    original_time = data[:, 0]
+    original_value = data[:, 1]
+
+    # 生成从起始时间到结束时间、间隔为 1 秒的时间数组
+    target_time = np.arange(
+        original_time[0],
+        original_time[-1] + 1,
+        1,
+    )
+
+    # 线性插值
+    target_value = np.interp(
+        target_time,
+        original_time,
+        original_value,
+    )
+
+    # 合并成 [time, value] 格式
+    return target_value
+
+
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from pathlib import Path
+from data_loader import load_attachment_one,interpolate_every_second
+from geometry import generate_geometry
+from heat_transfer import calculate_temperature_change
 
-from data_loader import load_attachment_one
 
 ATTACHMENT_ONE = Path(__file__).resolve().parents[2] / "附件" / "附件1.xlsx"
 
 
 def main() -> None:
+    #数据插值
     temperature_data, moisture_data = load_attachment_one(ATTACHMENT_ONE)
+    temperature = interpolate_every_second(temperature_data)
+    moisture = interpolate_every_second(moisture_data)
 
-    print(f"已读取 {len(temperature_data)} 个数据点")
-    print("温度数据 [time, temperature]：")
-    print(temperature_data)
-    print("水分数据 [time, moisture]：")
-    print(moisture_data)
+    print(f"温度：{temperature}")
+    print(len(temperature))
 
 
 if __name__ == "__main__":
